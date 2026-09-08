4b2537c1d83d535d9cb12762e65ad64fb425148b
mrcalzon02 <mrcalzon02@yahoo.com>
2026-09-08 11:33:21 -0800

    Add selection editor release marker

---
+++ modules/continuityworks_runtime/forge-1.20.1/src/main/java/io/continuityworks/blueprint/runtime/SelectionEditorClientHooks.java
@@ -0,0 +1,6 @@
+package io.continuityworks.blueprint.runtime;
+
+/** Marker class for release verification of the lower-left selection editor feature set. */
+final class SelectionEditorClientHooks {
+    private SelectionEditorClientHooks() {}
+}
