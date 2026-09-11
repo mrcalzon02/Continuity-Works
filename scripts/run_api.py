5ae0d31fa94819af64ec795384e4772db628216a
mrcalzon02 <mrcalzon02@yahoo.com>
2026-09-11 01:20:53 -0800

    Prefer canonical Continuity Works bind env

--- scripts/run_api.py
+++ scripts/run_api.py
@@ -10,6 +10,6 @@ serve(
         "CONTINUITY_WORKS_PROJECT_ROOT",
         os.environ.get("STRUCTURESMITH_PROJECT_ROOT", "."),
     ),
-    host=os.environ.get("HOST", "0.0.0.0"),
-    port=int(os.environ.get("PORT", "8787")),
+    host=os.environ.get("CONTINUITY_WORKS_HOST", os.environ.get("HOST", "0.0.0.0")),
+    port=int(os.environ.get("CONTINUITY_WORKS_PORT", os.environ.get("PORT", "8787"))),
 )
